9e84772bbd8d6a728d5c119f83129bdb8b9545e8
thecybereyes <81520821+thecybereyes@users.noreply.github.com>
2022-12-20 11:08:34 +0600

    Add files via upload

---
+++ cp.js
@@ -0,0 +1,30 @@
+function CheckPassword1(inputtxt) 
+{ 
+
+if(inputtxt.value=="f1") 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+} 
+
+function CheckPassword2(inputtxt) 
+{ 
+
+if(inputtxt.value=="f2") 
+{ 
+alert('Correct, try another...')
+return true;
+}
+else
+{ 
+alert('Wrong...!')
+return false;
+}
+}
+
