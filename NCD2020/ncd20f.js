1d616aeb886d82d198a841725a6504738894978b
thecybereyes <81520821+thecybereyes@users.noreply.github.com>
2022-12-21 00:20:29 +0600

    Update ncd20f.js

--- NCD2020/ncd20f.js
+++ NCD2020/ncd20f.js
@@ -94,3 +94,80 @@ return false;
 }
 }
 
+function CheckPassword_c2(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{WE_LOVE_YOU_LEO}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_c3(inputtxt) 
+{ 
+
+if(inputtxt.value=="DRILL2020{866fd58d77526c1bda8771b5b21d5b11}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_c4(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{ICTDAYBANGLADESH}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_c6(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{cirtdrillisawesome}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_c7(inputtxt) 
+{ 
+
+if(inputtxt.value=="SEC{fx343stdinstrcatoverflowsizeof}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+
+
