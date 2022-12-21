816ba8d9aa5d5266dc0444687217ded0786355a3
thecybereyes <81520821+thecybereyes@users.noreply.github.com>
2022-12-21 12:00:41 +0600

    Update ncd20f.js

--- NCD2020/ncd20f.js
+++ NCD2020/ncd20f.js
@@ -169,5 +169,219 @@ return false;
 }
 }
 
+// Forensic
+
+function CheckPassword_f1(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{b0559f600984ca7c0eab7be7d079f7ab}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f2(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{2006-07-09}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f3(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{JohnWasher}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f4(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{ACNESTICS}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f5(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{DC210}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f6(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{trout.exe}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f7(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{EYE}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f8(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{tooth}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f9(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{Cosine is an angle of 0}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f10(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{pf3.wav}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f12(inputtxt) 
+{ 
+
+if(inputtxt.value=="CyberDrill{incisorman420}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f13(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{12-02-08}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f14(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{S-1-5-21-3166329-3263506726-1320359247-1002}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+function CheckPassword_f15(inputtxt) 
+{ 
+
+if(inputtxt.value=="CYBERDRILL{Even Though We Consider The Distance,We Are Still Connected}") 
+{ 
+alert('Correct ! \n Thank You !')
+return true;
+}
+else
+{ 
+alert('Wrong ! \n Try Again !')
+return false;
+}
+}
+
+
+
 
 
